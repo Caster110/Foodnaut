3c06eb6da2ce20db537936fc7433186de900f29e
LostSckareDev <lostmaster423@gmail.com>
2023-11-05 15:58:45 +0700

    DEMO 0.0.0.1
    Сделано передвижение персонажа: может осматриваться, ходить, прыгать, бегать и приседать

---
+++ Foodnaut/Assets/Scripts/PlayerController/PlayerMouseLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 600f; //сенса мыши
+    public Transform playerBody;
+    private float xRotation = 0f;
+    void Start()
+    {
+        //блокировка курсора 
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //координаты мыши 
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        //вращение по осям X и Y
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //ограничение переворота камеры
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
