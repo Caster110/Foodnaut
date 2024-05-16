9845d17cdcb5a8a6ef4e1062f5c2c6abc2115228
LostSckareDev <lostmaster423@gmail.com>
2024-05-17 02:29:13 +0700

    0.0.2.3
    Добавил:
    - Подсветку контура предметов при наведении на них
    
    P.S.
    Почему-то контур багует со скрафченными предметами

--- Foodnaut/Assets/Scripts/Player/RaycastDetector.cs
+++ Foodnaut/Assets/Scripts/Player/RaycastDetector.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class RaycastDetector : MonoBehaviour
 {
@@ -7,10 +6,13 @@ public class RaycastDetector : MonoBehaviour
     [SerializeField] private Camera playerCamera;
     [SerializeField] private UIController inventoryUI;
     [SerializeField] private CrosshairUIController crosshairUIController;
+
     private Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
     private GameObject detectedObject;
+    private GameObject previousDetectedObject;
     private string detectedObjectType = null;
     private readonly string[] interactableTypes = {"CraftStation", "CollectibleItem", "Blender"};
+
     public GameObject GetDetectedObjectWithTag(string tagOfRequestedObject) 
     {
         if (detectedObjectType == tagOfRequestedObject)
@@ -18,31 +20,67 @@ public class RaycastDetector : MonoBehaviour
         else
             return null;
     }
+
     private void Update()
     {
-        detectedObject = TryDetect();
+        GameObject currentDetectedObject = TryDetect();
+
+        if (currentDetectedObject != detectedObject)
+        {
+            if (previousDetectedObject != null)
+            {
+                // Отключаем Outline на предыдущем объекте
+                Outline outlineScript = previousDetectedObject.GetComponent<Outline>();
+                if (outlineScript != null)
+                {
+                    outlineScript.enabled = false;
+                }
+            }
+
+            detectedObject = currentDetectedObject;
+
+            if (detectedObject != null)
+            {
+                Debug.Log(detectedObject);
+                // Включаем Outline на текущем объекте
+                Outline outlineScript = detectedObject.GetComponent<Outline>();
+                if (outlineScript != null)
+                {
+                    outlineScript.enabled = true;
+                }
+            }
+
+            // Обновляем предыдущий обнаруженный объект
+            previousDetectedObject = detectedObject;
+        }
     }
+
     private GameObject TryDetect()
     {
         if (inventoryUI.IsAnyClosableUIOpened())
             return null;
+
         bool isInteractableObject = false;
         detectedObjectType = null;
 
         Ray ray = playerCamera.ScreenPointToRay(screenCenter);
         RaycastHit hit;
-         
+
         if (Physics.Raycast(ray, out hit, reachDistance))
         {
             Transform target = hit.transform;
             foreach (string i in interactableTypes)
-                if (isInteractableObject = target.tag == i)
+            {
+                if (target.CompareTag(i))
                 {
                     detectedObjectType = target.tag;
+                    isInteractableObject = true;
                     crosshairUIController.TryChangeColor(isInteractableObject);
                     return target.gameObject;
                 }
+            }
         }
+
         crosshairUIController.TryChangeColor(isInteractableObject);
         return null;
     }
